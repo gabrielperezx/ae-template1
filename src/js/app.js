6ced1b7c2e0cb04d15bd5e463216c796c4790702
gabrielperezx <gabrieloctavioperez@gmail.com>
2025-10-23 00:49:52 -0400

    Se añadió la funcionalidad del carrusel y se mejoraron los estilos.
    - Se implementó Glider.js para carruseles con diferentes configuraciones para 3 diapositivas y 1 diapositiva.
    - Se mejoraron los estilos SCSS para los componentes del carrusel, incluyendo botones de navegación e indicadores.
    - Se introdujo un efecto de fondo de paralaje para la sección principal.
    - Se actualizaron varios estilos para lograr consistencia y mejorar el diseño en todos los componentes.

--- src/js/app.js
+++ src/js/app.js
@@ -13,11 +13,68 @@
         });
     };
 
+    const GliderCreator = () => {
+        const gliderConfig3Slides = {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            scrollLock: true,
+
+            responsive: [
+                {
+                    breakpoint: 769,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    },
+                },
+            ],
+        };
+
+        const gliderConfig1SlidesBase = {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            scrollLock: true,
+        };
+
+        document.querySelectorAll('.carousel__items').forEach((carousel) => {
+            const container = carousel.closest('.carousel');
+            const prevButton = container.querySelector('.carousel__anterior');
+            const nextButton = container.querySelector('.carousel__siguiente');
+            const dots = container.querySelector('.carousel__indicadores');
+
+            new Glider(carousel, {
+                ...gliderConfig3Slides,
+                arrows: {
+                    prev: prevButton,
+                    next: nextButton,
+                },
+                dots: dots,
+            });
+        });
+
+        document.querySelectorAll('.carousel__lista-1').forEach((carousel) => {
+            const container = carousel.closest('.carousel');
+            const prevButton = container.querySelector('.carousel__anterior');
+            const nextButton = container.querySelector('.carousel__siguiente');
+            const dots = container.querySelector('.carousel__indicadores');
+
+            new Glider(carousel, {
+                ...gliderConfig1SlidesBase,
+                arrows: {
+                    prev: prevButton,
+                    next: nextButton,
+                },
+                dots: dots,
+            });
+        });
+    };
+
     /*----- ----- ----- ----- -----
 	# Declaraciones
 	----- ----- ----- ----- -----*/
 
     window.addEventListener('load', () => {
         Tooltips();
+        GliderCreator();
     });
 })();
